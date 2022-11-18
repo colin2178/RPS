8d1f9eea6b87b78e0e725273f660f5943b4e294c
colin2178 <ashardalon78@hotmail.com>
2022-11-17 20:39:40 -0700

    next

--- rps.js
+++ rps.js
@@ -7,22 +7,26 @@ let input = prompt("Choose your fate!")
       // randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
 let nut = ['Rock', 'Paper', 'Scissors'];
 
- let getComputerChoice = nut[Math.floor(Math.random()*nut.length)];
-
-   console.log('The Computer choose:' + getComputerChoice); 
-
+ 
+//  let playerSelection = nut[Math.floor(Math.random()*nut.length)];
+//    console.log('The Computer choose:' + playerSelection); 
+let getComputerChoice = nut[Math.floor(Math.random()*nut.length)];
       // Write a function that plays a single round of Rock Paper Scissors.
       // The function should take two parameters - the "playerSelection" and 
       // "computerSelection" - and then return a string that declares the winner 
       // of the round like so: "You Lose! Paper beats Rock"
 
-// function playRound(playerSelection, computerSelection) {
-//     // your code here!
-//   }
-   
-// const playerSelection = "rock";
-// const computerSelection = getComputerChoice();
-// console.log(playRound(playerSelection, computerSelection));
+function playRound(playerSelection, computerSelection) {
+    if (computerSelection = 'Paper') 
+    result = "You Lose! Paper beats Rock!";  
+    return result;
+}
+  
+  
+
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice;
+console.log(playRound(playerSelection, computerSelection));
 
 
 
