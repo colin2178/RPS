e86c3203d9769d6367ecd035426e85d15105f9a5
colin2178 <ashardalon78@hotmail.com>
2022-11-17 18:01:01 -0700

    First part (done)

---
+++ rps.js
@@ -0,0 +1,53 @@
+    //Going to be played against comp
+
+       // Tip: use the console to make sure this is returning 
+      // the expected output before moving to the next step!
+let input = prompt("Choose your fate!")
+      // Begin with a function called getComputerChoice that will 
+      // randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
+let nut = ['Rock', 'Paper', 'Scissors'];
+
+ let getComputerChoice = nut[Math.floor(Math.random()*nut.length)];
+
+   console.log('The Computer choose:' + getComputerChoice); 
+
+      // Write a function that plays a single round of Rock Paper Scissors.
+      // The function should take two parameters - the "playerSelection" and 
+      // "computerSelection" - and then return a string that declares the winner 
+      // of the round like so: "You Lose! Paper beats Rock"
+
+// function playRound(playerSelection, computerSelection) {
+//     // your code here!
+//   }
+   
+// const playerSelection = "rock";
+// const computerSelection = getComputerChoice();
+// console.log(playRound(playerSelection, computerSelection));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
